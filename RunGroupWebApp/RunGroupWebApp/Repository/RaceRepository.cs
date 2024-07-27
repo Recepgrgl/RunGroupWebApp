38befa98c894b986a83130e02ad6f97337606f17
Recepgrgl <gurgillrecep@gmail.com>
2024-07-27 22:29:51 +0300

    RunGroupWebApp
    ASP.NET Core EntityFramework ile geliştirilmiştir.

---
+++ RunGroupWebApp/RunGroupWebApp/Repository/RaceRepository.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RunGroupWebApp.Data;
+using RunGroupWebApp.Interfaces;
+using RunGroupWebApp.Models;
+
+namespace RunGroupWebApp.Repository
+{
+    public class RaceRepository : IRaceRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RaceRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool Add(Race race)
+        {
+            _context.Add(race);
+            return Save();
+        }
+
+        public bool Delete(Race race)
+        {
+            _context.Remove(race);
+            return Save();
+        }
+
+        public async Task<IEnumerable<Race>> GetAll()
+        {
+            return await _context.Races.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Race>> GetAllRacesByCity(string City)
+        {
+            return await _context.Races.Where(c => c.Address.City.Contains(City)).ToListAsync();
+        }
+
+        public async Task<Race> GetByIdAsync(int id)
+        {
+            return await _context.Races.Include(i => i.Address).FirstOrDefaultAsync(x => x.Id == id);
+        }
+        public async Task<Race> GetByIdAsyncNoTracking(int id)
+        {
+            return await _context.Races.Include(i => i.Address).AsNoTracking().FirstOrDefaultAsync();
+        }
+
+        public bool Save()
+        {
+            var Saved = _context.SaveChanges();
+            return Saved > 0 ? true : false;
+        }
+
+        public bool Update(Race race)
+        {
+            _context.Update(race);
+            return Save();
+        }
+    }
+}
---
+++ RunGroupWebApp/RunGroupWebApp/ViewModels/CreateClubViewModel.cs
@@ -0,0 +1,17 @@
+﻿using RunGroupWebApp.Data.Enum;
+using RunGroupWebApp.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace RunGroupWebApp.ViewModels
+{
+    public class CreateClubViewModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Bu Alan Zorunlu"), MinLength(10)]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Address Address { get; set; }
+        public IFormFile Image { get; set; }
+    public ClubCategory ClubCategory { get; set; }
+    }
+}
